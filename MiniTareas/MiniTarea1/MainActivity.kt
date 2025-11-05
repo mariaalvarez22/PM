a8f534367ddeb81a7ea067202199cad0f7e0c5c0
mariaalvarez22 <mariaalvarezcosta10@gmail.com>
2025-11-05 13:26:45 +0100

    Create MainActivity.kt

---
+++ MiniTareas/MiniTarea1/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.minitarea1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // Ajuste de bordes para pantallas edge-to-edge
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Referencias a los elementos de la interfaz
+        val btnGuardar = findViewById<Button>(R.id.btnGuardar)
+        val etNombrePlanta = findViewById<EditText>(R.id.etNombrePlanta)
+
+        // Acción al pulsar el botón
+        btnGuardar.setOnClickListener {
+            val nombre = etNombrePlanta.text.toString()
+
+            if (nombre.isNotEmpty()) {
+                Toast.makeText(this, "Planta guardada: $nombre", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Por favor, introduce el nombre de la planta", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
